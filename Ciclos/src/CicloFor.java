3ac34fe8469ec71b5ae5aee11770832c680eb6ec
Leonardo M. <49843138+leomorales2083@users.noreply.github.com>
2025-04-17 22:04:41 +0000

    Ciclo For

---
+++ Ciclos/src/CicloFor.java
@@ -0,0 +1,8 @@
+public class CicloFor {
+    public static void main(String[] args) {
+        System.out.println("*** Ciclo FOR ***");
+        
+        for(var i = 1; i <= 20; i++)
+            System.out.println(i);
+    }
+}
---
+++ Ciclos/src/NumerosParesFor.java
@@ -0,0 +1,10 @@
+public class NumerosParesFor {
+    public static void main(String[] args) {
+        System.out.println("Numeros Pares Cicclo FOR");
+
+        for(var i = 0; i <= 20; i++){
+            if(i%2 == 0)
+                System.out.println(i);
+        }
+    }
+}
