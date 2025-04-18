919d851455cbf17fb5e22e30ce982c1e4c97a0b5
Leonardo M. <49843138+leomorales2083@users.noreply.github.com>
2025-04-18 15:30:19 +0000

    mejorando sistema CajeroAutomatico

--- Ciclos/src/CajeroAutomatico.java
+++ Ciclos/src/CajeroAutomatico.java
@@ -3,12 +3,10 @@ public class CajeroAutomatico {
         
         var consola = new java.util.Scanner(System.in);
         var salir = false;
-
-        var saldoInicial = 1000.00;
+        var saldo = 1000.00;
         var deposito = 0;
         var retiro = 0;
     
-
         while(!salir){
             System.out.print("""
                     *** Aplicación de Cajero Automático ***
@@ -17,28 +15,28 @@ public class CajeroAutomatico {
                     2. Retirar
                     3. Depositar
                     4. Salir
-                    Escoge una opción: \s """);
-             var opcion = consola.nextInt();
+                    Escoge una opción:\s""");
+                    var opcion = consola.nextInt();
              switch (opcion) {
-                case 1 -> System.out.printf("Tu Saldo es $%.2f \n\n",saldoInicial);
+                case 1 -> System.out.printf("Tu Saldo es $%.2f \n\n",saldo);
                 case 2 -> {
                     System.out.print("Ingresa el monto a retirar: $");
                     retiro = consola.nextInt();
-                    if(retiro > saldoInicial)
-                        System.out.println("El monto del retiro excede su saldo.. Saldo actual $" + saldoInicial);
+                    if(retiro > saldo)
+                        System.out.println("El monto del retiro excede su saldo.. Saldo actual $" + saldo);
                     else{
-                        saldoInicial -= retiro;
-                        System.out.printf("Tu nuevo Saldo es: $%.2f \n", saldoInicial);
+                        saldo -= retiro;
+                        System.out.printf("Tu nuevo Saldo es: $%.2f \n", saldo);
                     }
                 }
                 case 3 -> {
                     System.out.print("Ingresa el monto a depositar: $");
                     deposito = consola.nextInt();
-                    saldoInicial += deposito;
-                    System.out.printf("Tu nuevo Saldo es: $%.2f \n", saldoInicial);
+                    saldo += deposito;
+                    System.out.printf("Tu nuevo Saldo es: $%.2f \n", saldo);
                 }
                 case 4 ->{
-                    System.out.println("Saliendo del sistema...  ADIOS");
+                    System.out.println("Saliendo del sistema... Gracias por utilizar nuestros cajeros...");
                     salir = true;
                 }
                 default -> System.out.println("Opción Inválida....");
