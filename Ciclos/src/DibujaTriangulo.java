4d447386b3d62a6a9bda9047feba8b5136e77d72
Leonardo M. <49843138+leomorales2083@users.noreply.github.com>
2025-04-23 16:17:11 +0000

    Ultimo ejercicio ciclos for

--- Ciclos/src/DibujaTriangulo.java
+++ Ciclos/src/DibujaTriangulo.java
@@ -6,19 +6,14 @@ public class DibujaTriangulo {
 
         var consola = new Scanner(System.in);
 
-        System.out.println("Ingrese el numero de filas del triangulo: ");
-        var filas = consola.nextInt();
+        System.out.print("Ingrese el numero de filas del triangulo: ");
+        var numeroFilas = consola.nextInt();
 
-
-        for(var fila = 1; fila <= filas; fila ++){
-            for(var espacio = filas; espacio >= 1; espacio --){
-                System.out.println(" ");
-                for (var k = fila; k < filas; k=+2)
-                    System.out.print("*");
-            }
+        for(var fila = 1; fila <= numeroFilas; fila ++){
+            var espaciosBlanco = " ".repeat(numeroFilas - fila);
+            var asteriscos = "*".repeat(2 * fila -1);
+            System.out.printf("%s%s%n", espaciosBlanco, asteriscos);
         }
-
         consola.close();
-
     }
 }
