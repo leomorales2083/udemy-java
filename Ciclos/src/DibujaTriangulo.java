c53ac617e3fb5503bb90eb67ecc6e3d7231941ef
Leonardo M. <49843138+leomorales2083@users.noreply.github.com>
2025-04-27 22:54:12 +0000

    Revisión 270425

--- Ciclos/src/DibujaTriangulo.java
+++ Ciclos/src/DibujaTriangulo.java
@@ -12,7 +12,7 @@ public class DibujaTriangulo {
         for(var fila = 1; fila <= numeroFilas; fila ++){
             var espaciosBlanco = " ".repeat(numeroFilas - fila);
             var asteriscos = "*".repeat(2 * fila -1);
-            System.out.printf("%s%s%n", espaciosBlanco, asteriscos);
+            System.out.println(espaciosBlanco + asteriscos);
         }
         consola.close();
     }
