ae4a40bd789c9add3f75d673188c24b5208f90e4
Leonardo M. <49843138+leomorales2083@users.noreply.github.com>
2025-05-05 20:16:48 +0000

    Primer ejemplo de funciones

---
+++ Funciones/src/Funciones.java
@@ -0,0 +1,12 @@
+public class Funciones {
+
+    static void saludar(String mensaje){ //Esta función recibe un string con el nombre mensaje
+        System.out.println("Mensaje: " + mensaje); //se concatena con el valor encviado desde la función MAIN.
+    }
+
+    public static void main(String[] args) {
+        saludar("Hola Mundo");
+        
+    }
+    
+}
